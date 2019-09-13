203a2739cf0473c996d36617f7472af009faafd1
Neerav Karani <nkarani@ethz.ch>
2019-09-13 18:19:45 +0200

    initial commit

---
+++ experiments/unet.py
@@ -0,0 +1,43 @@
+import model_zoo
+import tensorflow as tf
+
+# ======================================================================
+# Model settings
+# ======================================================================
+model_handle = model_zoo.segmentation_cnn
+experiment_name = 'run3' 
+
+# ======================================================================
+# data settings
+# ======================================================================
+data_mode = '3D'
+image_size = [144, 112, 48] # [x, y, time]
+nchannels = 4 # [intensity, vx, vy, vz]
+nlabels = 2 # [background, foreground]
+
+# ======================================================================
+# training settings
+# ======================================================================
+max_epochs = 1000
+batch_size = 8
+learning_rate = 1e-3
+optimizer_handle = tf.train.AdamOptimizer
+loss_type = 'dice'  # crossentropy/dice
+summary_writing_frequency = 20
+train_eval_frequency = 320
+val_eval_frequency = 320
+save_frequency = 800
+
+continue_run = False
+debug = True
+augment_data = True
+
+# ======================================================================
+# test settings
+# ======================================================================
+# iteration number to be loaded after training the model (setting this to zero will load the model with the best validation dice score)
+#load_this_iter = 0
+#batch_size_test = 4
+#save_qualitative_results = True
+#save_results_subscript = 'initial_training_unet'
+#save_inference_result_subscript = 'inference_out_unet'
