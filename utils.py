d8f7effcfe723d39ed952da5ca831998bd9660a3
Neerav Karani <nkarani@ethz.ch>
2019-09-13 19:03:40 +0200

    added image normalization

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import os
 import glob
+import numpy as np
+from scipy.ndimage import gaussian_filter
     
 # ===================================================
 # ===================================================
@@ -24,3 +26,44 @@ def get_latest_model_checkpoint_path(folder, name):
 
     return os.path.join(folder, name + '-' + str(latest_iteration))
 
+# ==========================================        
+# function to normalize the input arrays (intensity and velocity) to a range between 0 to 1.
+# magnitude normalization is a simple division by the largest value.
+# velocity normalization first calculates the largest magnitude velocity vector
+# and then scales down all velocity vectors with the magnitude of this vector.
+# ==========================================        
+def normalize_image(image):
+
+    # ===============    
+    # initialize with zeros
+    # ===============
+    normalized_image = np.zeros((image.shape))
+    
+    # ===============
+    # normalize magnitude channel
+    # ===============
+    normalized_image[...,0] = image[...,0] / np.amax(image[...,0])
+    
+    # ===============
+    # normalize velocities
+    # ===============
+    
+    # extract the velocities in the 3 directions
+    velocity_image = np.array(image[...,1:4])
+    
+    # denoise the velocity vectors
+    velocity_image_denoised = gaussian_filter(velocity_image, 0.5)
+    
+    # compute per-pixel velocity magnitude    
+    velocity_mag_image = np.linalg.norm(velocity_image_denoised, axis=-1)
+    
+    # velocity_mag_array = np.sqrt(np.square(velocity_arrays[...,0])+np.square(velocity_arrays[...,1])+np.square(velocity_arrays[...,2]))
+    # find max value of 95th percentile (to minimize effect of outliers) of magnitude array and its index
+    vpercentile = np.percentile(velocity_mag_image, 95)    
+    normalized_image[...,1] = velocity_image_denoised[...,0] / vpercentile
+    normalized_image[...,2] = velocity_image_denoised[...,1] / vpercentile
+    normalized_image[...,3] = velocity_image_denoised[...,2] / vpercentile  
+    
+    # print('normalized arrays: max=' + str(np.amax(normalized_arrays)) + ' min:' + str(np.amin(normalized_arrays)))
+  
+    return normalized_image
